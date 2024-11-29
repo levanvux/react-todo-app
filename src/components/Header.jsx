b7818eea1ae66f7eaafb9dabe85cdb30a1f25051
Vu Le <hoanngu512@gmail.com>
2024-11-29 21:50:17 +0700

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 export const Header = React.memo((props) => {
     const { todos } = props;
+    const openTodos = todos.filter(todo => !todo.complete);
     return (
         <header>
-            <h1 className="text-gradient">You have {todos.length} open task{todos.length !== 1 ? "s" : ""}.</h1>
+            <h1 className="text-gradient">You have {openTodos.length} open task{openTodos.length !== 1 ? "s" : ""}.</h1>
         </header>
     );
 });
